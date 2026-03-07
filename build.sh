6e75e779c7b392a7d7b87683553263d8fdf822f6
compact-orb <compact-orb@outlook.com>
2026-03-07 20:44:49 +0800

    Remove build.sh from the zip archive

--- build.sh
+++ build.sh
@@ -21,7 +21,7 @@ do
 
 	echo
 
-	zip -9 -r -ll "$ZIP" META-INF/ build.sh customize.sh module.prop service.sh "$SCRIPT_NAME"
+	zip -9 -r -ll "$ZIP" META-INF/ customize.sh module.prop service.sh "$SCRIPT_NAME"
 done
 
 echo " * Done!"
